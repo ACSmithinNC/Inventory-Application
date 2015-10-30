61f45aaec939dce7178a886583e40aa0bff7e8de
ACSmithinNC <amy-christinesmith@msncom>
2015-10-30 12:11:53 -0400

    Update inventory_application.rb

--- inventory_application.rb
+++ inventory_application.rb
@@ -10,3 +10,19 @@ class Inventory_item
   end
 
 end
+def to_s
+  result = sprintf("%s: $%.2f (%d)", @name, @price, @quantity)
+  return result
+end
+
+asparagus = Inventory_item.new("Asparagus", 3.29, 100)
+carrots =  Inventory_item.new("Carrots", 2.99, 50)
+squash = Inventory_item.new("Squash", 2.59, 60)
+
+total_value = asparagus.price * asparagus.quantity +
+  carrots.price * carrots.quantity +
+  squash.price * squash.quantity
+
+printf("The inventory has a total value of $%.2f\n",
+  total_value)
+  
