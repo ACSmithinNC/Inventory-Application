6bb1410c732ba2e51845a454c339a2627e1e5823
benh704 <bhoff704@gmail.com>
2015-10-31 15:24:24 -0400

    Update inventory_application.rb

--- inventory_application.rb
+++ inventory_application.rb
@@ -1,28 +1,38 @@
-class Inventory_item
-  attr_accessor :name
-  attr_accessor :price
-  attr_accessor :quantity
+Inventory_item = {
+          "Asparagus" => 3.29,
+           "Carrots" => 2.99,
+           "Squash" => 2.59
+          }
+ Inventory_item.each do |name, price|
+   puts "#{name}: $#{price}"
+ end
 
-  def initialize(name, price, quantity)
-    @name = name
-    @price = price
-    @quantity = quantity
-  end
 
-end
-def to_s
-  result = sprintf("%s: $%.2f (%d)", @name, @price, @quantity)
-  return result
-end
+# class Inventory_item
+#   attr_accessor :name
+#   attr_accessor :price
+#   attr_accessor :quantity
 
-asparagus = Inventory_item.new("Asparagus", 3.29, 100)
-carrots =  Inventory_item.new("Carrots", 2.99, 50)
-squash = Inventory_item.new("Squash", 2.59, 60)
+#   def initialize(name, price, quantity)
+#     @name = name
+#     @price = price
+#     @quantity = quantity
+#   end
 
-total_value = asparagus.price * asparagus.quantity +
-  carrots.price * carrots.quantity +
-  squash.price * squash.quantity
+# end
+# def to_s
+#   result = sprintf("%s: $%.2f (%d)", @name, @price, @quantity)
+#   return result
+# end
 
-printf("The inventory has a total value of $%.2f\n",
-  total_value)
+# asparagus = Inventory_item.new("Asparagus", 3.29, 100)
+# carrots =  Inventory_item.new("Carrots", 2.99, 50)
+# squash = Inventory_item.new("Squash", 2.59, 60)
+
+# total_value = asparagus.price * asparagus.quantity +
+#   carrots.price * carrots.quantity +
+#   squash.price * squash.quantity
+
+# printf("The inventory has a total value of $%.2f\n",
+#   total_value)
   
