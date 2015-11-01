11e07675006a226aa627d138dfe084c4d623f340
ACSmithinNC <amy-christinesmith@msncom>
2015-11-01 16:17:31 -0500

    Update inventory_application.rb

--- inventory_application.rb
+++ inventory_application.rb
@@ -1,28 +1,56 @@
-class Inventory_item
-  attr_accessor :name
-  attr_accessor :price
-  attr_accessor :quantity
+Inventory_item = {
+     "Asparagus" => 3.29,
+     "Carrots" => 2.99,
+     "Squash" => 2.59
+   }
+#
+# class Inventory_item
+# attr_accessor :name
+# attr_accessor :price
+# attr_accessor :quantity
 
-  def initialize(name, price, quantity)
-    @name = name
-    @price = price
-    @quantity = quantity
-  end
 
+
+# -  attr_accessor :name        +          "Asparagus" => 3.29,
+# -  attr_accessor :price        +           "Carrots" => 2.99,
+# -  attr_accessor :quantity        +           "Squash" => 2.59
+# +          }
+
+Inventory_item.each do |name, price|
+puts "#{name}: $#{price}"
 end
-def to_s
-  result = sprintf("%s: $%.2f (%d)", @name, @price, @quantity)
-  return result
+
+
+# + Inventory_item.each do |name, price|
+# +   puts "#{name}: $#{price}"
+# + end
+
+keylist = []
+price_hash = { }
+
+input = ""
+veggie = ""
+price = 0
+
+print "Enter vegetable, price; or just ENTER to quit: "
+input = gets.chomp
+while input != "" do
+(veggie, price) = input.split(",")
+price = price.to_f
+
+price_hash[veggie] = price
+
+print "Enter vegetable, price; or just ENTER to quit: "
+input = gets.chomp
 end
 
-asparagus = Inventory_item.new("Asparagus", 3.29, 100)
-carrots =  Inventory_item.new("Carrots", 2.99, 50)
-squash = Inventory_item.new("Squash", 2.59, 60)
+puts "Here are the vegetable contents:"
+price_hash.each do |key,value|
+puts "#{key} are this much #{value}."
+end
 
-total_value = asparagus.price * asparagus.quantity +
-  carrots.price * carrots.quantity +
-  squash.price * squash.quantity
-
-printf("The inventory has a total value of $%.2f\n",
-  total_value)
-  
+puts "Here are the vegetable contents, sorted into order:"
+keylist = price_hash.keys.sort
+keylist.each do |key|
+puts "#{key} is #{price_hash[key]}."
+end
